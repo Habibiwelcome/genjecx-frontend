55424856eb154f750b64f0580b024ee810f0b5ec
Habibiwelcome <2023ebcs195@online.bits-pilani.ac.in>
2026-01-03 19:10:48 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: { unoptimized: true }
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/genjecx-frontend",
+  assetPrefix: "/genjecx-frontend/",
 };
 
 export default nextConfig;
